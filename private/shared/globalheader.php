d24881d4f3dfe86a40d90f5f755502ba59958712
Ben <ware.benjamin@oakwoodschools.org>
2020-01-17 17:57:00 -0500

    Navbar continued work

--- private/shared/globalheader.php
+++ private/shared/globalheader.php
@@ -2,80 +2,37 @@
 <html>
 <head>
 	<title>Christopher's Restaurant</title>
-	
 	<!--Room for stylesheets and PHP -->
-
-	<!--Temporary Styling until sheets are made -->
-<style> 
-html {
-background-color:black;
-}
-
-.navBar a{
-  float: left;
-  display: block;
-  color: white;
-  text-align: center;
-  margin: 3% 8%;
-  width:9%;
-  text-decoration: none;
-  font-size: 30px;
-  }
-  
-#menu {
-margin-right:12%;
-margin-left:4%;
-}
-#order {
-margin-right:4%;
-margin-left:12%;
-}
-
-.navLogo {
-position:absolute;
-width:20%;
-margin:0% 40%;
-overflow:hidden;
-}
-img.navImg { 
-	width:100%;
-	height: 100%;
-	}
-
-
-.sticky {
-  position: fixed;
-  top: 0;
-  width:100%;
-}
-</style>
+	<link rel="stylesheet" type="text/css" href="../private/shared/styles/styles.css"> <!--Waiting for dynamic php statements -->
 </head>
 <body>
 	<header>
 		<!--Nav bar is at the top of every page -->
 		<div class="navWrapper"> <!--Includes entire nav system-->
 			<div class="navBar" id="navBar"> <!--Just Includes the navbar which will float with the page-->
-					<a id="home" href=HomePagePlaceholder>Home</a>
-					<a id="menu" href=MenuPagePlaceholder>Menu</a>
-					<a id="order"href=OrdersPagePlaceholder>Order</a>
-					<a id="bag"href=BagPagePlaceholder>Bag</a>
+					<a id="home" href=HomePagePlaceholder>Home</a> <!--Waiting for dynamic php statements -->
+					<a id="menu" href=MenuPagePlaceholder>Menu</a> <!--Waiting for dynamic php statements -->
+					<a id="order"href=OrdersPagePlaceholder>Order</a> <!--Waiting for dynamic php statements -->
+					<a id="bag"href=BagPagePlaceholder>Bag</a> <!--Waiting for dynamic php statements -->
 			</div>
 			<div class="navLogo"><!--Includes Christopher's Icon logo -->
-				<img class="navImg" src="chrisLogo.png">
+				<img class="navImg" src="../private/shared/images/chrisLogo.png"> <!--Waiting for dynamic php statements -->
 			</div>
 		</div>
 	
 		<script>
-window.onscroll = function() {myFunction()};
+			//If the page scrolls down the navbar transitions to sticky version
+		window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navBar");
-var sticky = navbar.offsetTop;
+		var navbar = document.getElementById("navBar");
+		var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
+		function myFunction() {
+ 			 if (window.pageYOffset >= sticky + 50) { 
+  			  navbar.classList.add("sticky")
+  			} else {
+    			navbar.classList.remove("sticky");
+			
   }
 }
 </script>
