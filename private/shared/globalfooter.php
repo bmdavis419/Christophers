0a73438f2c6888ad37b68fad2d5dc388c41fc07c
Ben <ware.benjamin@oakwoodschools.org>
2020-01-25 12:33:15 -0500

    Header and footer changes

--- private/shared/globalfooter.php
+++ private/shared/globalfooter.php
@@ -1,24 +1,26 @@
-</body>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <footer>
-	<center><h2>Christopher's Restaurant</h2></center>
+<div class="footerMap">
+    <div class="mapouter">
+		<div class="gmap_canvas">
+			<iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Christophers&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.org">embedgooglemap.org</a>
+		</div>
+	</div>
+</div>
+<div class="footerContent">
+	<h2>Christopher's Restaurant</h2>
 	<address>
-		<center>2318 DOROTHY LANE, KETTERING, OH, 45420<br></center>
-		<center><a href="tel:9372990089">937-299-0089</a><br></center>
-		<center><a href="mailto:info@christophers.biz">INFO@CHRISTOPHERS.BIZ</a><br>
+		<p>2318 DOROTHY LANE, KETTERING, OH, 45420<p>
+		<a href="http://christopherscatering.biz">Catering Website</a><br>
+		<a href="tel:9372990089">937-299-0089</a><br>
+		<a href="mailto:info@christophers.biz">Email Here</a><br>
 	</address>
 	<div class="socials">
 		<a href="facebook.com"  class="fa fa-facebook"></a>
 		<a href="twitter.com"  class="fa fa-twitter"></a>
 		<a href="instagram.com"  class="fa fa-instagram"></a>
 	</div>
-	<br>
-	<center><a href="http://christopherscatering.biz">Catering</a></center>
-	<br>
-	<style type="text/css">
-	footer {
-		border: 3px solid black;
-	}
-	</style>
+</div>
 </footer>
 </body>
 </html>
