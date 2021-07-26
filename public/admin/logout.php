23e173954543bd637dcd02fb20c386a725a95799
Phoenix41902 <bmdavis419@protonmail.com>
2021-07-26 17:09:57 -0400

    changed to node

--- public/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['valid'])&& ($_SESSION['valid'] = true)){
-    // unset the session and leave
-    session_unset();
-    header("Location: index.php");
-}
-?>
