1b603d9ae4826ba46de2e4ad9dacd412400f11b8
Benjamin Davis <bmdavis419@protonmail.com>
2021-07-27 21:42:18 -0400

    asdf

--- website/pages/about.tsx
+++ website/pages/about.tsx
@@ -1,5 +1,16 @@
+import { GetServerSideProps } from "next";
 import React from "react";
 
-export default function about() {
-	return <div>about</div>;
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // ...
+  return {
+    props: {
+      topText: "lmao jaja text",
+    },
+  };
+};
+
+export default function about(props: { topText: string }) {
+  const { topText } = props;
+  return <div>{topText}</div>;
 }
