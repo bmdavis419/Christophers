38dbcb74168dde66020971c9e5160ecbe6b71c90
Ben Ware <benwareohio@gmail.com>
2021-08-10 20:52:07 -0400

    responsive index

--- website/components/index/IndexFeatures.tsx
+++ website/components/index/IndexFeatures.tsx
@@ -27,6 +27,16 @@ export default function IndexFeatures(props: PropsInterface) {
 		bottomLink:"/",
 		image:"LogoRes.jpg",
     },
+    {
+        id:"2",
+        title:"Not Perfect Chicken Dinner",
+        description:"Our Chicken dinners are cooked to perfection and will make you come back wishing for more. I am just writing text to fill this out. Content, this is content",
+        topLinkText:"Order Online",
+		topLink:"/",
+		bottomLinkText:"Bottom Link",
+		bottomLink:"/",
+		image:"default.jpg",
+    }
 
 
     
@@ -43,15 +53,15 @@ export default function IndexFeatures(props: PropsInterface) {
     }
 
     return (
-        <div className="overflow-hidden relative md:rounded-50px w-full max-h-144 h-144 grid sm:grid-cols-2 justify-evenly filter drop-shadow-xl bg-gray-50">
-            <div className="h-144 relative w-full"><Image layout="fill" objectFit="fill" src={`/${tempContent.homepageFeatures[index].image}`} alt="Image of featured dish" /></div>
+        <div className="overflow-hidden relative md:rounded-50px w-full max-h-144 h-96 sm:h-144 grid sm:grid-cols-2 justify-evenly filter drop-shadow-xl bg-gray-50">
+            <div className="h-144 relative w-full hidden sm:relative"><Image layout="fill" objectFit="fill" src={`/${tempContent.homepageFeatures[index].image}`} alt="Image of featured dish" /></div>
             <div className="align-center h-144 text-white sm:text-black sm:pt-12 bg-black bg-opacity-60 sm:bg-transparent sm:bg-opacity-100 p-4 space-y-2 xl:space-y-4 2xl:space-y-4 h-4/5vw flex flex-col text-center lg:w-2/3 mx-auto">
-                <Image className="max-h-144 relative visible sm:invisible mix-blend-multiply" objectFit="fill" layout="fill" src="/LogoRes.jpg" alt="Menu Header Background Image"/>
+                <Image className="max-h-144 relative visible sm:invisible mix-blend-multiplg" objectFit="fill" layout="fill" src={`/{tempContent.homepageFeatures[index].image}`} alt="This text"/>
                 <h1 className="relative mix-blend normal text-3xl font-bold">{tempContent.homepageFeatures[index].title}</h1>
                 <p className="relative mix-blend normal text-sm xl:text-xl">{tempContent.homepageFeatures[index].description}</p>
                 <div className="grid grid-cols-1 2xl:grid-cols-2 gap-2 2xl:gap-16">
                 <button className="relative mix-blend normal w-40  m-auto 2xl:text-xl rounded-full py-2 2xl:py-2 bg-primary text-white">{tempContent.homepageFeatures[index].topLinkText}</button>
-                <button className="relative mix-blend normal w-40  m-auto 2xl:text-xl rounded-full py-2 2xl:py-2 bg-primary text-white">{tempContent.homepageFeatures[index].topLinkText}</button>
+                <button className="relative mix-blend normal w-40  m-auto 2xl:text-xl rounded-full py-2 2xl:py-2 bg-primary text-white">{tempContent.homepageFeatures[index].bottomLinkText}</button>
                 </div>
                 <div className="relative mix-blend normal flex flex-row self-end mx-auto">{circles}</div>
             </div>
