51d3b94a67c242fa5fe0e732859dde01f056ebc7
Ben Ware <benwareohio@gmail.com>
2021-08-13 00:20:36 -0400

    contact forms

--- website/pages/contact.tsx
+++ website/pages/contact.tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from 'next';
 import React from 'react'
 import client from '../apollo-client';
 import ContactForm from '../components/contact/ContactForm'
+import ContactCateringForm from '../components/contactCatering/ContactCateringForm';
 import Info from '../components/index/Info'
 
 interface PropsInterface {
@@ -49,8 +50,8 @@ export const getServerSideProps: GetServerSideProps = async () => {
 export default function contact(props:PropsInterface) {
     const {restaurantInfo} = props
     return (
-        <div className="flex justify-center align-center">
-            <ContactForm />
+        <div className="flex justify-center flex-col lg:flex-row align-center">
+            <div className="lg:mx-2 mx-auto"><ContactForm /></div>
             <div className="max-w-144"><Info restaurantInfo={restaurantInfo}/></div>
         </div>
     )
